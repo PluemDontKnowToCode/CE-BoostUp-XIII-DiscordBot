6dadaad245dcc741c70348b7b7752e9542eff014
_MeePooh_ <122891690+PluemDontKnowToCode@users.noreply.github.com>
2025-06-06 15:51:13 +0700

    Update SheetFile.js
    Remove debug

--- service/SheetFile.js
+++ service/SheetFile.js
@@ -35,7 +35,6 @@ export async function ReadSheetAndSave() {
   try {
     const res = await fetch(url);
     const text = await res.text();
-    console.log(text)
     const json = JSON.parse(text.substring(47).slice(0, -2));
     const col = json.table.cols;
     const colHead = [];
